a64e1e5366eea7382f6384baa626f53ce3f3efad
Yoemny González Almaguer <yoemny@gmail.com>
2017-08-09 16:41:27 +0100

    try to run in heroku

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 
-var port = 3000;
+app.set('port', (process.env.PORT || 3000));
 
 var app = express();
 
 //View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view_engine', 'ejs');
+app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 // set static folder
@@ -23,5 +23,5 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', index);
 
 app.listen(port, function(){
-    console.log('Server started on port '+port);
+    console.log('Server started on port ' + app.get('port'));
 });
