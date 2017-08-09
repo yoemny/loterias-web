788e34d520c6b2db75caaf58ee22047ec04964fd
Yoemny González Almaguer <yoemny@gmail.com>
2017-08-09 16:25:12 +0100

    basic node.js

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var port = 3000;
+
+var app = express();
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view_engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+
+app.listen(port, function(){
+    console.log('Server started on port '+port);
+});
